64c7758b2d8909d0207e131480d5f69a834ec6bd
budicmarin <55134480+budicmarin@users.noreply.github.com>
2020-11-12 15:37:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace budicMarinPonavljanjeZadatak6ž
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] niz=new int[100];
+            Console.WriteLine("Upisuj brojeve za niz, a za kraj pritisni k ");
+            char k;
+            int i = 0;
+            k = Console.ReadKey().KeyChar;
+            while (k!='k')
+            {
+                niz[i] = Convert.ToInt32(k);
+                i++;
+                k = Console.ReadKey().KeyChar;
+            }
+            Array.Sort(niz);
+            foreach (int broj in niz)
+            {
+                Console.WriteLine(broj);
+            }
+
+        }
+    }
+}
